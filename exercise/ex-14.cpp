5f1b6fd9ba386b3265e82da01222439ad3dd0e22
cat2koban <cat2koban@users.noreply.github.com>
2020-10-21 18:59:26 +0900

    Add ex-14

---
+++ exercise/ex-14.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int A, B, C;
+    cin >> A >> B >> C;
+
+    int max_num = max(max(A, B), C);
+    int min_num = min(min(A, B), C);
+
+    cout << abs(max_num - min_num) << endl;
+}
